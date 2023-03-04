10d39a02e68b4d55a89e4025cebeeab8eaadb8ef
shogo4131 <shougo08697118@gmail.com>
2023-03-05 00:33:53 +0900

    feat: urqlの設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css';
+import 'ress';
 import type { AppProps } from 'next/app';
 
+import { Provider } from 'urql';
+
+import { client } from '@/libs/client';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 };
 
 export default MyApp;
