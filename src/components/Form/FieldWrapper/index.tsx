38ba3a310c332f8a00bc8a99eba2f73f0e5e46b4
shogo4131 <shougo08697118@gmail.com>
2023-03-14 02:02:30 +0900

    feat: フィールドラッパーにエラーpropsを追加

--- src/components/Form/FieldWrapper/index.tsx
+++ src/components/Form/FieldWrapper/index.tsx
@@ -1,14 +1,20 @@
 import type { FC, ReactNode } from 'react';
 
+import type { FieldError } from 'react-hook-form';
+
 import styles from './index.module.css';
 
+// TODO: 型の生成を調べる
+type ErrorMessage = Pick<FieldError, 'message'>;
+
 type Props = {
+  errror?: ErrorMessage;
   children: ReactNode;
   label: string;
   htmlFor: string;
 };
 
-export const FieldWrapper: FC<Props> = ({ children, label, htmlFor }) => {
+export const FieldWrapper: FC<Props> = ({ errror, children, label, htmlFor }) => {
   return (
     <div>
       <label className={styles.label} htmlFor={htmlFor}>
@@ -16,6 +22,7 @@ export const FieldWrapper: FC<Props> = ({ children, label, htmlFor }) => {
         <span className={styles.required}>*</span>
       </label>
       {children}
+      <div className={styles.error}>{errror?.message && errror.message}</div>
     </div>
   );
 };
