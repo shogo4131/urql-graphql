5f84ba3ef6c34a13949795e13f213f95ff1c2976
shogo4131 <shougo08697118@gmail.com>
2023-03-14 01:24:55 +0900

    feat: 新規登録画面にバリデーションの追加

--- src/features/auth/pages/RegisterPage/index.tsx
+++ src/features/auth/pages/RegisterPage/index.tsx
@@ -1,5 +1,9 @@
-/* eslint-disable no-console */
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import z from 'zod';
+
 import { Layout } from '@/features/auth/components/Layout';
+import { useRegisterMutation } from '@/graphql/generated/graphql';
 
 import { Button } from '@/components/Elemets/Button';
 import { FieldWrapper } from '@/components/Form/FieldWrapper';
@@ -7,21 +11,52 @@ import { InputField } from '@/components/Form/InputField';
 
 import styles from './index.module.css';
 
+const schema = z
+  .object({
+    name: z.string().min(1, '名前は必須です。').trim(),
+    email: z.string().email('書式に誤りがあります。').min(1, 'メールアドレスは必須です。'),
+    password: z.string().min(1, 'パスワードを入力してください。'),
+    passwordConfirm: z.string().min(1, '確認パスワードを入力してください。'),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: 'パスワードが一致しません。',
+    path: ['passwordConfirm'],
+  });
+
+type Schema = z.infer<typeof schema>;
+
 export const RegisterPage = () => {
+  const [, register] = useRegisterMutation();
+
+  const { register: registerForm, handleSubmit } = useForm<Schema>({
+    resolver: zodResolver(schema),
+  });
+
+  const onClickRegisterHandler: SubmitHandler<Schema> = async (data) => {
+    await register({
+      registerInput: {
+        username: data.name,
+        email: data.email,
+        password: data.password,
+        confirmPassword: data.passwordConfirm,
+      },
+    });
+  };
+
   return (
     <Layout title="新規登録">
-      <form>
+      <form onSubmit={handleSubmit(onClickRegisterHandler)}>
         <FieldWrapper label="名前" htmlFor="name">
-          <InputField type="text" id="name" onChange={() => console.log(888)} />
+          <InputField type="text" id="name" registration={registerForm('name')} />
         </FieldWrapper>
         <FieldWrapper label="メールアドレス" htmlFor="email">
-          <InputField type="email" id="email" onChange={() => console.log(888)} />
+          <InputField type="email" id="email" registration={registerForm('email')} />
         </FieldWrapper>
         <FieldWrapper label="パスワード" htmlFor="password">
-          <InputField type="password" id="password" onChange={() => console.log(888)} />
+          <InputField type="password" id="password" registration={registerForm('password')} />
         </FieldWrapper>
         <FieldWrapper label="パスワード確認" htmlFor="confirm">
-          <InputField type="password" id="confirm" onChange={() => console.log(888)} />
+          <InputField type="password" id="confirm" registration={registerForm('passwordConfirm')} />
         </FieldWrapper>
         <div className={styles.registerButton}>
           <Button type="submit">登録</Button>
