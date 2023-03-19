cba4f6303135145aae9eef46ac84d64ea1911a98
shogo4131 <shougo08697118@gmail.com>
2023-03-19 22:59:36 +0900

    feat: tokenの有効期限をチェックして無効ならリダイレクトする処理を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,36 @@
 import '../styles/globals.css';
 import 'ress';
+import { useCallback, useEffect } from 'react';
+
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 import { Provider } from '@/providers';
+import { isProtectedRoute, isTokenExpired } from '@/utils/token';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const { events, push, asPath } = useRouter();
+
+  const handleRouteChange = useCallback(
+    (url: string) => {
+      const token = localStorage.getItem('token');
+      const invalid = token ? isTokenExpired(token) : true;
+
+      if (invalid && isProtectedRoute(url)) {
+        push('/login');
+      }
+    },
+    [push]
+  );
+
+  useEffect(() => {
+    // NOTE: 初回レンダリング時のtokenのチェックを行う
+    handleRouteChange(asPath);
+
+    events.on('routeChangeStart', handleRouteChange);
+    return () => events.off('routeChangeStart', handleRouteChange);
+  }, [asPath, events, handleRouteChange]);
+
   return (
     <Provider>
       <Component {...pageProps} />
