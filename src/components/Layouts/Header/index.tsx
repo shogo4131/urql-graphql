c011709161c3bb149be158af7b8e871a04dfb765
shogo4131 <shougo08697118@gmail.com>
2023-03-19 15:27:07 +0900

    feat: ログアウトの処理を追加

--- src/components/Layouts/Header/index.tsx
+++ src/components/Layouts/Header/index.tsx
@@ -1,18 +1,27 @@
 import Image from 'next/image';
 
 import LogoutIcon from '@/assets/exit.svg';
+import { useAuth } from '@/providers/authProvider';
 
 import styles from './index.module.css';
 
 export const Header = () => {
+  const { user, logoutHandler } = useAuth();
+
+  // TODO: API作ったら修正
+  const onClickLogoutHandler = () => {
+    logoutHandler();
+    window.location.assign('/login');
+  };
+
   return (
     <header className={styles.root}>
       <div>
         <h1>テストアプリ</h1>
       </div>
       <div className={styles.user}>
-        <div>ユーザー名</div>
-        <button type="button">
+        <div>{user}</div>
+        <button type="button" onClick={onClickLogoutHandler}>
           <Image src={LogoutIcon} alt="ログアウト" />
         </button>
       </div>
